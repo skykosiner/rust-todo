1363a24a0c4a415026305c508a56c5159cce1e1c
Yoni Kosiner <yoni@kosiner.co.uk>
2022-01-25 15:52:24 +0000

    aoeu

--- target/debug/deps/rust_todo-26d45d352d16b24a.d
+++
@@ -1,7 +0,0 @@
-/home/yoni/personal/rust-todo/target/debug/deps/rust_todo-26d45d352d16b24a.rmeta: src/main.rs
-
-/home/yoni/personal/rust-todo/target/debug/deps/rust_todo-26d45d352d16b24a.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
--- target/debug/rust-todo.d
+++
@@ -1,1 +0,0 @@
-/home/yoni/personal/rust-todo/target/debug/rust-todo: /home/yoni/personal/rust-todo/src/main.rs
